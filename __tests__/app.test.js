5aad810899d6d836a8b4a045ba3a97620f277f5b
yhdchan <yhdchan1993uk@gmail.com>
2022-09-08 11:18:03 +0100

    Add comment count property to select-article-by-id with test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,22 +27,40 @@ describe('GET /api/topics', () => {
 })
 
 describe('GET /api/articles/:article_id', () => {
-	test('200: should return an article object which should have the properties including author, title, article_id, body, topic, created_at and votes',() => {
-		return request(app)
-			.get('/api/articles/1')
-			.expect(200)
-			.then(({ body }) => {
-				expect(body.article).toEqual({
-					article_id: 1,
-					title: "Living in the shadow of a great man",
-    			topic: "mitch",
-   				author: "butter_bridge",
-    			body: "I find this existence challenging",
-    			created_at: "2020-07-09T20:11:00.000Z",
-   				votes: 100,
-				})
-			})
-	})
+	test('200: should return an article object which should have the properties including author, title, article_id, body, topic, created_at, votes and comment_count that greater than 0',() => {
+    return request(app)
+      .get('/api/articles/1')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          comment_count: 11,
+        })
+      })
+  })
+  test('200: should return an article object which should have the properties including author, title, article_id, body, topic, created_at, votes and comment_count that article_id exists but no comment so far',() => {
+    return request(app)
+      .get('/api/articles/7')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual({
+          article_id: 7,
+          title: "Z",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "I was hungry.",
+          created_at: "2020-01-07T14:08:00.000Z",
+          votes: 0,
+          comment_count: 0,
+        })
+      })
+  })
 	test('404: should return an error message when passed an article id that does not exist', () => {
 		return request(app)
 			.get('/api/articles/1000')
