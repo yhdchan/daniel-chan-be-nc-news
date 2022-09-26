c8ba1a7cf1b5afca4efc8a503ca1cf0c437daf41
yhdchan <yhdchan1993uk@gmail.com>
2022-09-26 11:52:08 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const { 
 	handleCustomErrors, 
@@ -9,6 +10,7 @@ const apiRouter = require('./routes/api.router');
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.use('/api', apiRouter);
